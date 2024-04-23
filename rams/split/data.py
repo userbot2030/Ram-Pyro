47bb0dd60157b1e8b7f92c019df9cd0caccb5bc8
userbot2030 <166553732+userbot2030@users.noreply.github.com>
2024-04-24 02:09:51 +0700

    data.py

--- rams/split/data.py
+++ rams/split/data.py
@@ -4,7 +4,7 @@ modules = CMD_HELP
 class Data:
 
     text_help_menu = (
-        f"**Menu Inline Ram-Pyro**\n**Perintah :** ? ! . * , $"
+        f"**Menu Inline Dan-Pyro**\n**Perintah :** ? ! . * , $"
         .replace(",", "")
         .replace("[", "")
         .replace("]", "")
